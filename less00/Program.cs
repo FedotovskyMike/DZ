a4c585298edaae2a09015006e8831c0344c9f263
Fedotovsky M <fedotovsky.m@yandex.ru>
2022-06-20 20:15:18 +0300

    начало урока

---
+++ less00/Program.cs
@@ -0,0 +1,50 @@
+﻿/* void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] mass = new int[12];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-9, 10);
+}
+Print(mass); */
+
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] mass = new int[12];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-9, 10);
+}
+Print(mass);
+
+int sumNeg = 0;
+int sumPos = 0;
+for (int i = 0; i < mass.Length; i++)
+{
+    if (mass[i] > 0)
+    {
+        sumPos += mass[i];
+    }
+    else
+    {
+        sumNeg += mass[i];
+    }
+}
+Console.WriteLine(sumPos);
+Console.WriteLine(sumNeg);
+
+
